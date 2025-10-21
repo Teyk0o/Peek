4df489645d3d031f107741c246484a1fcdd9f4b4
Teyk0o <theovilainfr@gmail.com>
2025-10-21 15:31:38 +0200

    feat: setup project structure with modern Win32 GUI framework
    - Add CMakeLists with Win32 dependencies
    - Create network monitoring module header
    - Create modern GUI module header with dark mode support
    - Add logger system for console output
    - Include complete .gitignore for CLion/C project

---
+++ gui.h
@@ -0,0 +1,39 @@
+/*
+* PEEK - Network Monitor
+*/
+
+#ifndef PEEK_GUI_H
+#define PEEK_GUI_H
+
+#include <windows.h>
+#include <commctrl.h>
+#include "network.h"
+
+#pragma comment(lib, "comctl32.lib")
+#pragma comment(lib, "uxtheme.lib")
+#pragma comment(lib, "dwmapi.lib")
+
+#define COLOR_BG_DARK       RGB(30, 30, 30)
+#define COLOR_BG_LIGHT      RGB(45, 45, 45)
+#define COLOR_TEXT_PRIMARY  RGB(240, 240, 240)
+#define COLOR_TEXT_SECONDARY RGB(180, 180, 180)
+#define COLOR_ACCENT        RGB(0, 120, 212)
+#define COLOR_SUCCESS       RGB(16, 185, 129)
+#define COLOR_WARNING       RGB(245, 158, 11)
+#define COLOR_ERROR         RGB(239, 68, 68)
+
+int gui_init(HINSTANCE hInstance);
+
+HWND gui_create_window(HINSTANCE hInstance);
+
+int gui_run(void);
+
+void gui_add_connection(const NetworkConnection* conn);
+
+void gui_update_stats(const NetworkStats* stats);
+
+void gui_clear_list(void);
+
+void gui_enable_dark_mode(HWND hwnd);
+
+#endif
---
+++ network.h
@@ -0,0 +1,51 @@
+/*
+* PEEK - Network Monitor
+*/
+
+#ifndef PEEK_NETWORK_H
+#define PEEK_NETWORK_H
+
+#include <windows.h>
+#include <winsock2.h>
+#include <ws2tcpip.h>
+#include <iphlpapi.h>
+
+#pragma comment(lib, "iphlpapi.lib")
+#pragma comment(lib, "ws2_32.lib")
+
+#define MAX_CONNECTIONS 2000
+#define MAX_PROCESS_NAME 260
+
+typedef struct {
+    DWORD local_addr;
+    DWORD local_port;
+    DWORD remote_addr;
+    DWORD remote_port;
+    DWORD pid;
+    DWORD state;
+    char process_name[MAX_PROCESS_NAME];
+    SYSTEMTIME timestamp;
+} NetworkConnection;
+
+typedef struct {
+    int total_connections;
+    int new_connections;
+    int active_connections;
+    int initial_connections;
+} NetworkStats;
+
+int network_init(void);
+
+void network_cleanup(void);
+
+int network_get_connections(NetworkConnection** connections, int* count);
+
+int network_check_new_connections(NetworkConnection** new_connections, int* count);
+
+void network_get_stats(NetworkStats* stats);
+
+void network_format_ip(DWORD addr, char* buffer, size_t size);
+
+void network_get_process_name(DWORD pid, char* buffer, size_t size);
+
+#endif
